8aa312425fb5d7714dc93379ab7b1f950fdaf49d
Tanya Yubko <webtormashki@gmail.com>
2020-05-24 19:25:22 +0300

    do desctop version

---
+++ src/js/script.js
@@ -0,0 +1,16 @@
+// window.onload=function(){
+// 	var menuElem = document.getElementById('dropdown-menu'),
+// 		titleElem = menuElem.querySelector('.dashed');
+// 		document.onclick = function(event) {
+// 		var target = elem = event.target;
+// 		while (target != this) {
+// 			  if (target == menuElem) {
+// 			  if(elem.tagName == 'A') titleElem.innerHTML = elem.textContent;
+// 			  menuElem.classList.toggle('open')
+// 				  return;
+// 			  }
+// 			  target = target.parentNode;
+// 		  }
+// 		menuElem.classList.remove('open');
+// 	}
+// 	}
